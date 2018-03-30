003f26f2b9817fb7507ec3a61ec19ad152fe5dca
Kevin Schulte <kschultee@gmail.com>
2018-03-30 14:52:35 -0700

    Added CSS classes to input area React class #6

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ class APInput extends React.Component {
   render() {
     return (
       <div className="input-group">
+        <span className="input-group-label primary">API Key:</span>
         <input className="input-group-field" type="text" placeholder='API Key here...' onChange={this.props.handleChange}></input>
         <div className="input-group-button">
           <input type="submit" className="button" onClick={this.props.handleSubmit}></input>
