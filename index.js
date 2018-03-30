d45fd1b89ed7ecb7b88bd70ed28daeaa61adf8c0
Kevin Schulte <kschultee@gmail.com>
2018-03-30 14:17:39 -0700

    Added functionality to input area allowing users to input their own api key #6

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ import ReactDOM from 'react-dom'
 const SECTIONS = ['home', 'opinion', 'world', 'national', 'politics', 'upshot', 'nyregion', 'business', 'technology', 'science', 'health', 'sports', 'arts', 'books', 'movies', 'theater', 'sundayreview', 'fashion', 'tmagazine', 'food', 'travel', 'magazine', 'food', 'travel', 'magazine', 'realestatem', 'automobiles', 'obituaries', 'insider']
 const apiURL = 'https://api.nytimes.com/svc/topstories/v2/'
 const apiSECTION = 'home.json'
-const apiKEY = '?api-key=af2ca657204e4b729d70302b9a7c17a2'
-const buildAPI = new Request(apiURL + apiSECTION + apiKEY)
+const apiKEY = '?api-key='
+const buildAPI = (apiURL + apiSECTION + apiKEY)
 class APInput extends React.Component {
   render() {
     return (
       <div className="input-group">
-        <input className="input-group-field" type="text" placeholder='API Key here...'></input>
+        <input className="input-group-field" type="text" placeholder='API Key here...' onChange={this.props.handleChange}></input>
         <div className="input-group-button">
-          <input type="submit" className="button" value="Submit"></input>
+          <input type="submit" className="button" onClick={this.props.handleSubmit}></input>
         </div>
       </div>
     )
@@ -63,20 +63,31 @@ class FilteredPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      apiInput: '',
       result: []
     }
     this.handleSelectChange = this.handleSelectChange.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleInputSubmit = this.handleInputSubmit.bind(this)
   }
-  handleSelectChange(e) {
-    fetch(apiURL + e.target.value + '.json' + apiKEY)
+  handleInputChange(e) {
+    this.setState({
+      apiInput: e.target.value
+    })
+  }
+  handleInputSubmit() {
+    fetch(buildAPI + this.state.apiInput)
       .then(res => res.json())
       .then(res =>
         this.setState({
           result: res.results
         }))
+      .catch(err =>
+        console.log(err)
+      )
   }
-  componentDidMount() {
-    fetch(buildAPI)
+  handleSelectChange(e) {
+    fetch(apiURL + e.target.value + '.json' + apiKEY + this.state.apiInput)
       .then(res => res.json())
       .then(res =>
         this.setState({
@@ -94,7 +105,10 @@ class FilteredPage extends React.Component {
                 onSelectChange={this.handleSelectChange}
               />
             </span>
-            <APInput />
+            <APInput
+              handleChange={this.handleInputChange}
+              handleSubmit={this.handleInputSubmit}
+            />
           </div>
         </div>
         <div>
