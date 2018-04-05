898e265960f70804c7d8d0c199becd9ff6f96cf2
Kevin Schulte <kschultee@gmail.com>
2018-04-05 14:03:46 -0700

    Added js file for React Article component #10

---
+++ components/Saved.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Saved extends React.Component {
+  render() {
+    return (
+      <div className = 'saved'>
+        {this.props.saved.map(result => (
+          <div className='callout' id={result.title} key = {result.title}>
+            <div className='media-object'>
+              <div className='media-object-section'>
+                <div className='thumbnail'>
+                  {result.multimedia.length > 0 ? <img src={result.multimedia[0].url} /> : <img src='img/NYT_thumbnail.jpg'/>}
+                </div>
+              </div>
+              <div className='media-object-section'>
+                <h4>{result.title}</h4>
+                <h6>{result.byline}</h6>
+                <p>{result.abstract}</p>
+              </div>
+            </div>
+            <button type='button' className='button alert' onClick={ () => this.props.onRemoveCall(result)} style={{margin: 0}}>Clear</button>
+            <a className='button' href={result.url}>View</a>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default Saved
