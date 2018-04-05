e03e57922c9e5869771ebc37f2536abce20c721d
Kevin Schulte <kschultee@gmail.com>
2018-04-05 13:57:15 -0700

    Added js file for React APInput component

---
+++ components/APInput.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class APInput extends React.Component {
+  render() {
+    return (
+      <div className="input-group">
+        <span className="input-group-label primary">API Key:</span>
+        <input className="input-group-field" type="text" placeholder='API Key here...' onChange={this.props.handleChange}></input>
+        <div className="input-group-button">
+          <input type="submit" className="button" onClick={this.props.handleSubmit}></input>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default APInput
